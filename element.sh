ce3c099b8d5aa9d2701346046be444f2434f0ce2
Abede2412 <abdulrahman241296@gmail.com>
2023-02-28 10:26:59 +0000

    feat: add kondisi jika argumen kosong

--- element.sh
+++ element.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+PSQL="psql -X -U freecodecamp -d periodic_table --tuples-only -c"
+NOT_FOUND="I could not find that element in the database."
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ELEMENTS=$($PSQL "select atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements inner join properties using(atomic_number) inner join types using(type_id) where atomic_number = $1")
+    if [[ -z $ELEMENTS ]]
+    then
+      echo $NOT_FOUND
+    else
+      echo "$ELEMENTS"
+    fi
+  fi
+fi
+
+
+
+
